2b1214ea39c829f134077d9e5096ec39953ca45d
Albert Fernandez Marsal <albert.fernandez@king.com>
2018-12-04 10:27:46 +0100

    More groovy

--- src/main/groovy/Day1.groovy
+++ src/main/groovy/Day1.groovy
@@ -1,24 +1,22 @@
-
 class Day1 {
 
     static void main(String... args) {
     }
 
     static def test1(String input) {
-        return input.readLines().inject(0) { result, i -> result + i.toInteger() }
+        return input.readLines().sum { it.toInteger() }
     }
 
     static def test2(String input) {
         def counter = 0
         def frequencies = [counter] as Set
-        while(true) {
-            for (String line : input.readLines() ) {
+        while (true) {
+            for (String line : input.readLines()) {
                 def integer = line.toInteger()
                 counter += integer
-                if (counter in frequencies) {
+                if (!frequencies.add(counter)) {
                     return counter
                 }
-                frequencies << counter
             }
         }
     }
