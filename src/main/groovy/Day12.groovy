647ee2a9aaf4a9eef204b66988c6396efdf36b7d
Albert Fernandez Marsal <albert.fernandez@king.com>
2018-12-13 22:52:48 +0100

    Day12.2

--- src/main/groovy/Day12.groovy
+++ src/main/groovy/Day12.groovy
@@ -1,49 +1,43 @@
 class Day12 {
 
-`    static final long GENERATIONS = 20
-//    static final long GENERATIONS = 20000
-//    static final long GENERATIONS = 50000000000
-    static final int RULE_SIZE = 4
-    static final int OFFSET = RULE_SIZE
-    static final String PRE_AND_SUFFIX = "." * OFFSET
+    static def test1(final String input, long generations) {
 
-    static def test1(final String input) {
-
-        def lines = input.readLines()
-
-        def matcher = lines[0] =~ /initial state: ([#\\.]*)/
-        if (!matcher) throw new RuntimeException("Invalid first line")
-        String pots
-        int dummy
-        int current0Index
-        (pots, current0Index) = normalizePots(new StringBuilder(matcher[0][1]), dummy)
-
-        Map<String, String> rules = lines.drop(2).collectEntries {
-            def m2 = it =~ /([#\\.]{5}) => ([#\\.])/
-            if (!m2) throw new RuntimeException("Invalid first line")
-            [m2[0][1], m2[0][2]]
-        }
-
+        def potConfigs = [:] as HashMap
+        def (String pots, long current0Index, Map<String, String> rules) = parseInput(input)
         long it = 0
-        while (++it <= GENERATIONS) {
-            if (it % 100_000 == 0) println "Processing $it..."
+        while (++it <= generations) {
             def newPots = new StringBuilder("." * pots.length())
             for (int idx = 2; idx < pots.size() - 2; ++idx) {
                 def partToMatch = pots[(idx - 2)..(idx + 2)]
                 newPots.setCharAt(idx, rules.getOrDefault(partToMatch, ".").toCharacter())
             }
             (pots, current0Index) = normalizePots(newPots, current0Index)
-//            println "${sprintf("%2d", [it])}> idx: $current0Index, pots:${" " * (10 - current0Index)} ${pots}"
+            def previousIndex = potConfigs.putIfAbsent(pots, current0Index)
+//            println "${sprintf("%2d", [it])}> idx: $current0Index, pots: ${pots}"
+            if (previousIndex != null) {
+                current0Index -= (generations - it)
+                break
+            }
         }
-//        println pots
-        long result = 0L
-        pots.eachWithIndex { String ch, int i ->
-            if (ch == "#") result += (i - current0Index)
-        }
-        result
+        sumPots(pots, current0Index)
     }
 
-    static def normalizePots(final StringBuilder sb, int curr0Index) {
+    static def parseInput(String input) {
+        def lines = input.readLines()
+
+        def matcher = lines[0] =~ /initial state: ([#\\.]*)/
+        if (!matcher) throw new RuntimeException("Invalid first line")
+        def (pots, current0Index) = normalizePots(new StringBuilder(matcher[0][1]), 0)
+
+        Map<String, String> rules = lines.drop(2).collectEntries {
+            def m2 = it =~ /([#\\.]{5}) => ([#\\.])/
+            if (!m2) throw new RuntimeException("Invalid first line")
+            [m2[0][1], m2[0][2]]
+        }
+        [pots, current0Index, rules]
+    }
+
+    static def normalizePots(final StringBuilder sb, long curr0Index) {
         while (sb[0] == '.') {
             sb.deleteCharAt(0)
             --curr0Index
@@ -53,5 +47,13 @@ class Day12 {
         while (sb[sb.length() - 1] == ".") sb.deleteCharAt(sb.length() - 1)
         sb.append("....")
         [sb.toString(), curr0Index]
+    }
+
+    private static long sumPots(String pots, long current0Index) {
+        long result = 0L
+        pots.eachWithIndex { String ch, long i ->
+            if (ch == "#") result += (i - current0Index)
+        }
+        result
     }
 }
--- src/test/groovy/Day12Test.groovy
+++ src/test/groovy/Day12Test.groovy
@@ -4,12 +4,21 @@ class Day12Test extends Specification {
 
     def "test 1"() {
         expect:
-            Day12.test1(input) == output
+            Day12.test1(input, 20) == output
 
         where:
             input        | output
             SAMPLE_INPUT | 325
             FINAL_INPUT  | 3337
+    }
+
+    def "test 2"() {
+        expect:
+            Day12.test1(input, 50_000_000_000) == output
+
+        where:
+            input        | output
+            FINAL_INPUT  | 4300000000349
     }
 
     static final SAMPLE_INPUT = '''\
